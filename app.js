6cb97436d3249881e8ff3d37520946994786c8fd
Rustam Kuzyaev <xpyct405@yandex.ru>
2023-03-02 13:59:43 +0300

    feat: change code (click out of modal window) && add styles

--- app.js
+++ app.js
@@ -20,20 +20,12 @@ closeBtn.onclick = function() {
   modal.style.display = "none";
 }
 
-
-// Обработчик события клика вне окна!
-// Функция при клике по модальному окну modal в любое место кроме картинки, изменяет стиль на display: none у modal
-modal.onclick = function (e) {
-  // Ниже условие которое проверяет на какую область был onclick, и если на область которая включает modalImg, т.е. картинку, то возвращает true, иначе false
-  // includes() определяет, содержит ли массив определённый элемент, возвращая в зависимости от этого true или false.
-	const withinBoundaries = e.composedPath().includes(modalImg);
-  console.log(withinBoundaries)
-
-	if ( ! withinBoundaries ) {
-		modal.style.display = 'none'; // скрываем элемент т к клик был за его пределами
-	}
+// Обработчик клика вне окна
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none"
+  }
 }
-
 
 // если картинок много то 
 // let img = document.querySelectorAll('.myImg');
@@ -44,4 +36,18 @@ modal.onclick = function (e) {
 //     modalImg.src = this.src;
 //     captionText.innerHTML = this.alt; 
 //   })
+// }
+
+// Обработчик события клика вне окна! 
+// Старая версия
+// Функция при клике по модальному окну modal в любое место кроме картинки, изменяет стиль на display: none у modal
+// modal.onclick = function (e) {
+  // Ниже условие которое проверяет на какую область был onclick, и если на область которая включает modalImg, т.е. картинку, то возвращает true, иначе false
+  // includes() определяет, содержит ли массив определённый элемент, возвращая в зависимости от этого true или false.
+// 	const withinBoundaries = e.composedPath().includes(modalImg);
+//   console.log(withinBoundaries)
+
+// 	if ( ! withinBoundaries ) {
+// 		modal.style.display = 'none'; // скрываем элемент т к клик был за его пределами
+// 	}
 // }
